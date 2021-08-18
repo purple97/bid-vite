4945fd987d4d2698bf23a9fdbad549bb0dbc3204
PurpleDragonfly <10940221@qq.com>
2021-08-18 10:58:54 +0800

    改为babel打包

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@
  * 未经本人允许禁止商业用途
  */
 
-// export { default as Utils } from './utils/index';
+export { default as Utils } from './utils/index';
 export { default as getIp } from './utils/get-ip';
-// export { default as Lint } from './actions/lint';
-// export { default as Version } from './actions/version';
-// export { IconfontToBase64 } from './actions/iconfont';
-// export { LessToCss } from './actions/less-to-css';
+export { default as Lint } from './actions/lint';
+export { default as Version } from './actions/version';
+export { IconfontToBase64 } from './actions/iconfont';
+export { LessToCss } from './actions/less-to-css';
 export { default as CompareVersion } from './utils/compare-version';
